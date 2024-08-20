fee299a0333117f61e766e5325ab924259c910d8
WolfgangNiehues <Wolfga.niehue24@edu.bztg-oldenburg.de>
2024-08-20 15:17:33 +0200

    Update calculator.py
    Mit Funktion
    kg in g
    cm in zoll
    kW in W

--- calculator.py
+++ calculator.py
@@ -1,30 +1,48 @@
-#Eingabefenster Bediener
+#Umrechnung
+def kg_in_gramm(kg):
+    return kg * 1000
 
-print("\n Auswahlmenü")
-print("1. kg->g")
-print("2. cm->zoll")
-print("3. Bar->Pascal")
-print("4. ????")
-print("5. break")
+def cm_in_zoll(cm):
+    return cm * 2,54
 
-if input: 1
+def kW_in_W(kW):
+    return kW * 1000
 
-1 == float(input())
-kg = float(input("Gib die erste Zahl für kg ein: "))
+#Bediener Auswahl
+def main():
+    while True:
+        print("Wählen Sie eine Umrechnungsmöglichkeit:")
+        print("1. kg zu gr")
+        print("2. cm zu Zoll")
+        print("3. kW zu W")
+        print("4. Beenden")
 
-2 == float(input())
-cm = float(input("Gib die erste Zahl für cm ein: "))
+#Auswahlaufforderung
+        choice = input("Geben Sie die Nummer Ihrer Wahl ein: ")
 
-3 == float(input())
-Bar = float(input("Gib die erste Zahl für Bar ein: "))
+#variable Berechnung
+        if choice == '1':
+            kg = float(input("Geben Sie die kg ein: "))
+            g = kg_in_gramm(kg)
+            print(f"{kg} Kilogramm sind {g} Gramm.\n")
+        
+        elif choice == '2':
+            cm = float(input("Geben Sie die cm ein: "))
+            zoll = cm_in_zoll(cm)
+            print (f"{cm} cm sind {zoll} Zoll. \n")
+            
+        elif choice == '3':
+            kW = float(input("Geben Sie die kW ein: "))
+            W = kW_in_W(kW)
+            print (F"{kW} kw sind {W} W.")
 
-#4 == float(input())
-#kg = float(input("Gib die erste Zahl ein: "))
+#Schleife unterbrechen
+        elif choice == '4':
+            print("Programm beendet.")
+            break
+        else:
+            print("Ungültige Wahl. Bitte wählen Sie eine gültige Option.\n")
 
-#5 == float(input())
-#kg = float(input("Gib die erste Zahl ein: "))
-
-
-
-
-
+#Menüauswahl
+if __name__ == "__main__":
+    main()
