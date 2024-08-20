9af1647ba14980060318a2216a1d2b9094f5b78f
WolfgangNiehues <Wolfga.niehue24@edu.bztg-oldenburg.de>
2024-08-20 11:19:53 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,20 +1,30 @@
 #Eingabefenster Bediener
-def menue():
-    while True:
-        print("\n Auswahlmenü")
-        print("1. kg->g")
-        print("2. cm->zoll")
-        print("3. Bar->Pascal")
-        print("4. ????")
-        print("5. break")
 
-1 == kg
-#2 = cm
-#3 = Bar
-#4 = ???
-#5 = !break
+print("\n Auswahlmenü")
+print("1. kg->g")
+print("2. cm->zoll")
+print("3. Bar->Pascal")
+print("4. ????")
+print("5. break")
 
 if input: 1
 
+1 == float(input())
+kg = float(input("Gib die erste Zahl für kg ein: "))
 
-kg == float(input ("Bitte Wert für kg angeben"))
+2 == float(input())
+cm = float(input("Gib die erste Zahl für cm ein: "))
+
+3 == float(input())
+Bar = float(input("Gib die erste Zahl für Bar ein: "))
+
+#4 == float(input())
+#kg = float(input("Gib die erste Zahl ein: "))
+
+#5 == float(input())
+#kg = float(input("Gib die erste Zahl ein: "))
+
+
+
+
+
