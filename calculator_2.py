ca8405955754c684df009ddb732379e31cb03035
WolfgangNiehues <Wolfga.niehue24@edu.bztg-oldenburg.de>
2024-08-28 17:19:19 +0200

    Update calculator_2.py

--- calculator_2.py
+++ calculator_2.py
@@ -3,7 +3,7 @@ def bed_eing_ausw ():
     check_bed = False
     while check_bed == False:
         bed_eing = input("\n Auswahl: ")
-        if bed_eing in ["1", "2" , "3" , "4" , "5"]:
+        if bed_eing in ["1", "2" , "3" , "4" , "5" , "6" , "7"]:
             check_bed = True
         else:
             print(" ungültige Eingabe\
@@ -17,12 +17,12 @@ def float_auswahl_wert():
         aus_wahl_wert = input ("\n Wert: ")
         aus_wahl_wert=aus_wahl_wert.replace("," , ".")
         try:
-            aus_wahl_wert = float (aus_wahl_wert)
+            aus_wahl_wert_float = float (aus_wahl_wert)
             check_ausw = True
         except ValueError:
             print(" ungültige Eingabe\
                  \n Bitte geben Sie einen Wert an, welcher umgerechnet werden kann")
-    return aus_wahl_wert
+    return aus_wahl_wert_float
 
 while True:
     
@@ -32,37 +32,60 @@ while True:
             \n 2: cm -> m \
             \n 3: °C -> °F \
             \n 4: °F -> °C \
-            \n 5: beenden")
+            \n 5: g -> uncen\
+            \n 6: uncen -> g\
+            \n 7: beenden")
     
 #Check korrekte Menue- Auswahl, weitergabe an case
     Menue_eingabe = bed_eing_ausw()
-
+    
+    if Menue_eingabe == '7':
+        print(" auf wiedersehen")
+        break
+    
 #Bediener Eingabe Umrechnen
     aus_wahl_gepr = float_auswahl_wert()
 
 #Berechnung und Ausgabe
     match Menue_eingabe:
+        
+        #cm -> zoll
         case '1':
             ergebniss = aus_wahl_gepr/2.54
             ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
             print(f"\n {aus_wahl_gepr}cm sind {ergebniss} Zoll")
         
+        #cm -> m
         case '2':
             ergebniss = aus_wahl_gepr/100
             ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
             print(f"{aus_wahl_gepr}cm sind {ergebniss} m")
         
+        #°C -> °F
         case '3':
             ergebniss = (aus_wahl_gepr * 9/5) + 32
             ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
             print(f" {aus_wahl_gepr}°C sind {ergebniss} °F")
         
+        #°F -> °C
         case '4':
             ergebniss = aus_wahl_gepr* 9/5 + 32
             ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
             print(f" {aus_wahl_gepr}°F sind {ergebniss} °C")
-
         
+        #g -> uncen
+        case '5':
+            ergebniss = aus_wahl_gepr/28.3495231
+            ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
+            print(f" {aus_wahl_gepr}g sind {ergebniss} uncen")
+        
+        #uncen -> g
+        case '6':
+            ergebniss = aus_wahl_gepr*28.3495231
+            ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
+            print(f" {aus_wahl_gepr}uncen sind {ergebniss} g")
+
+
  
 
         
