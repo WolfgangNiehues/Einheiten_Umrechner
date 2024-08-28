02d9653772aed182b80e0867b567acce037ff46f
WolfgangNiehues <Wolfga.niehue24@edu.bztg-oldenburg.de>
2024-08-28 16:44:34 +0200

    Update calculator_2.py

--- calculator_2.py
+++ calculator_2.py
@@ -1,30 +1,31 @@
-while True:
 # str->float
-    def bed_eing_ausw ():
-        check_bed = False
-        while check_bed == False:
-            bed_eing = input("\n Auswahl: ")
-            if bed_eing in ["1", "2" , "3" , "4" , "5"]:
-                check_bed = True
-            else:
-                print(" ungültige Eingabe\
-                     \n bitte wählen Sie eine der vorgeschlagenen Zahlen")
-        return bed_eing
+def bed_eing_ausw ():
+    check_bed = False
+    while check_bed == False:
+        bed_eing = input("\n Auswahl: ")
+        if bed_eing in ["1", "2" , "3" , "4" , "5"]:
+            check_bed = True
+        else:
+            print(" ungültige Eingabe\
+                    \n bitte wählen Sie eine der vorgeschlagenen Zahlen")
+    return bed_eing
     
 # Eingabe Umrechnungswerte float, Komma zu Punkt und runden nach 3 Dezimalstellen
-    def float_auswahl_wert():
-        check_ausw = False
-        while check_ausw == False:
-            aus_wahl_wert = input ("\n Wert: ")
-            aus_wahl_wert=aus_wahl_wert.replace("," , ".")
-            try:
-                aus_wahl_wert = float (aus_wahl_wert)
-                check_ausw = True
-            except ValueError:
-                print(" ungültige Eingabe\
+def float_auswahl_wert():
+    check_ausw = False
+    while check_ausw == False:
+        aus_wahl_wert = input ("\n Wert: ")
+        aus_wahl_wert=aus_wahl_wert.replace("," , ".")
+        try:
+            aus_wahl_wert = float (aus_wahl_wert)
+            check_ausw = True
+        except ValueError:
+            print(" ungültige Eingabe\
                  \n Bitte geben Sie einen Wert an, welcher umgerechnet werden kann")
-        return aus_wahl_wert
+    return aus_wahl_wert
 
+while True:
+    
 # Bedienereingabe Menü
     print("	\n Wähe aus zwischen: \
             \n 1: cm -> zoll \
