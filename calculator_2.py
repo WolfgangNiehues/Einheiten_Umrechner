3955721ca8f1be066a7288451214d18c6dfc26a0
WolfgangNiehues <Wolfga.niehue24@edu.bztg-oldenburg.de>
2024-09-10 17:53:37 +0200

    Korrektur
    Rechtschreibung
    Kommentare

--- calculator_2.py
+++ calculator_2.py
@@ -1,9 +1,9 @@
-# str->float
+# Bediener_eingabe_auswahl
 def bed_eing_ausw ():
     check_bed = False
     while check_bed == False:
         bed_eing = input("\n Auswahl: ")
-        if bed_eing in ["1", "2" , "3" , "4" , "5" , "6" , "7"]:
+        if bed_eing in ["0", "1", "2" , "3" , "4" , "5" , "6"]:
             check_bed = True
         else:
             print(" ungültige Eingabe\
@@ -34,13 +34,13 @@ while True:
             \n 4: °F -> °C \
             \n 5: g -> uncen\
             \n 6: uncen -> g\
-            \n 7: beenden")
+            \n 0: beenden")
     
 #Check korrekte Menue- Auswahl, weitergabe an case
     Menue_eingabe = bed_eing_ausw()
 
 #Beenden
-    if Menue_eingabe == '7':
+    if Menue_eingabe == '0':
         print(" auf wiedersehen")
         break
     
@@ -52,39 +52,39 @@ while True:
         
         #cm -> zoll
         case '1':
-            ergebniss = aus_wahl_gepr/2.54
-            ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
-            print(f"\n {aus_wahl_gepr}cm sind {ergebniss} Zoll")
+            ergebnis = aus_wahl_gepr/2.54
+            ergebnis = round (ergebnis ,3) #ergebnis wird gerundet überschrieben
+            print(f"\n {aus_wahl_gepr}cm sind {ergebnis} Zoll")
         
         #cm -> m
         case '2':
-            ergebniss = aus_wahl_gepr/100
-            ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
-            print(f"{aus_wahl_gepr}cm sind {ergebniss} m")
+            ergebnis = aus_wahl_gepr/100
+            ergebnis = round (ergebnis ,3) #ergebnis wird gerundet überschrieben
+            print(f"{aus_wahl_gepr}cm sind {ergebnis} m")
         
         #°C -> °F
         case '3':
-            ergebniss = (aus_wahl_gepr * 9/5) + 32
-            ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
-            print(f" {aus_wahl_gepr}°C sind {ergebniss} °F")
+            ergebnis = (aus_wahl_gepr * 9/5) + 32
+            ergebnis = round (ergebnis ,3) #ergebnis wird gerundet überschrieben
+            print(f" {aus_wahl_gepr}°C sind {ergebnis} °F")
         
         #°F -> °C
         case '4':
-            ergebniss = (aus_wahl_gepr-32)*5/9
-            ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
-            print(f" {aus_wahl_gepr}°F sind {ergebniss} °C")
+            ergebnis = (aus_wahl_gepr-32)*5/9
+            ergebnis = round (ergebnis ,3) #ergebnis wird gerundet überschrieben
+            print(f" {aus_wahl_gepr}°F sind {ergebnis} °C")
         
         #g -> uncen
         case '5':
-            ergebniss = aus_wahl_gepr/28.3495231
-            ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
-            print(f" {aus_wahl_gepr}g sind {ergebniss} uncen")
+            ergebnis = aus_wahl_gepr/28.3495231
+            ergebnis = round (ergebnis ,3) #ergebnis wird gerundet überschrieben
+            print(f" {aus_wahl_gepr}g sind {ergebnis} uncen")
         
         #uncen -> g
         case '6':
-            ergebniss = aus_wahl_gepr*28.3495231
-            ergebniss = round (ergebniss ,3) #ergebniss wird gerundet überschrieben
-            print(f" {aus_wahl_gepr}uncen sind {ergebniss} g")
+            ergebnis = aus_wahl_gepr*28.3495231
+            ergebnis = round (ergebnis ,3) #ergebnis wird gerundet überschrieben
+            print(f" {aus_wahl_gepr}uncen sind {ergebnis} g")
 
 
  
